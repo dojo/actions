e681cf4b66aa1f21293041fd4c0a46fef19dc338
Kitson Kelly <me@kitsonkelly.com>
2016-05-19 15:36:14 +0100

    Update dojo-compose

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core.d.ts" />
 /// <reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts" />
-/// <reference path="../node_modules/dojo-compose/typings/dojo-compose/dojo-compose.d.ts" />
+/// <reference path="../node_modules/dojo-compose/dist/umd/dojo-compose.d.ts" />
 /// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader.d.ts" />
 /// <reference path="../tests/typings/node/node.d.ts" />
 /// <reference path="../node_modules/@reactivex/rxjs/typings/main/ambient/es6-shim/index.d.ts" />
